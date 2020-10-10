c5d8396914f8bc8dde8f838adbc526586e08373a
Matěj Gargula <gargumat@fel.cvut.cz>
2020-10-10 21:08:59 +0200

    Simple movement and Sandbox
    Added tileset tools, player and player movement.
    Created simple sandbox.

---
+++ hry_project/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Config
+    [SerializeField] float movementSpeed = 5f;
+
+    // Cached variables
+
+    Vector2 moveDirection;
+
+    // Cached components
+    Rigidbody2D rigidBody;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProccesInputs();
+        HandleMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProccesInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rigidBody.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed);
+    }
+
+    void HandleMovement()
+    {
+        if (moveDirection.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if( moveDirection.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+}
