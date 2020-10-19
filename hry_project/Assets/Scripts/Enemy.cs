2971f8644ca7adafbb6772ebfa8479cbc89e123e
Lukáš Zahradník <zahralu5@fel.cvut.cz>
2020-10-20 00:42:37 +0200

    Add dummy enemy

---
+++ hry_project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 300f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
