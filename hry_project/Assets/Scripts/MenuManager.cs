a66b5e0afaf3715e34b1a680d1536fca89ac4140
Cyril Janecek <janeccyr@fel.cvut.cz>
2020-10-21 15:53:04 +0200

    Create MainMenu scene

---
+++ hry_project/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MenuManager : Singleton<MenuManager>
+{
+    public void Play()
+    {
+        print("Play");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
