85fe37ea77806f6218918472b53cce1134241c07
Cyril Janecek <janeccyr@fel.cvut.cz>
2020-10-21 13:47:49 +0200

    Create Singleton class and make GameManager a Singleton

--- hry_project/Assets/GameManager.cs
+++ hry_project/Assets/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     private bool isGamePaused = false; 
 
