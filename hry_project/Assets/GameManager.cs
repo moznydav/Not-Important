0ff89ce6a7bd8c0cf0241f17c8731aea86d535a8
Cyril Janecek <janeccyr@fel.cvut.cz>
2020-10-21 13:27:34 +0200

    Add basic GameManager with game pausing

---
+++ hry_project/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isGamePaused = false; 
+
+    void Pause()
+    {
+        isGamePaused = true;
+        Time.timeScale = 0f;
+    }
+
+    void Resume()
+    {
+        isGamePaused = false;
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+}
