7682b539ccdc71778f7fa7cdbf2e84ab7c79ace6
Lukáš Zahradník <zahralu5@fel.cvut.cz>
2020-11-05 23:35:34 +0100

    Make grid dynamic

--- hry_project/Assets/Scripts/Constants.cs
+++ hry_project/Assets/Scripts/Constants.cs
@@ -7,4 +7,5 @@ public static class Constants
 	public const int ENEMY_LAYER = 1 << 11;
 	public const string PLAYER_TAG = "Player";
 	public const string ASTAR_TAG = "PathFinding";
+	public const string DESTROYABLE_TAG = "Destroyable";
 }
---
+++ hry_project/Assets/Scripts/Destroyable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Destroyable : MonoBehaviour
+{
+    AStar pathfinding;
+
+    void Start()
+    {
+        pathfinding = (AStar) GameObject.FindWithTag(Constants.ASTAR_TAG).GetComponent(typeof(AStar));
+        var cell = pathfinding.WorldToCell(transform.position);
+
+        pathfinding.SetWall(cell.Item1, cell.Item2, true);
+    }
+
+    public void Destroy()
+    {
+        var cell = pathfinding.WorldToCell(transform.position);
+        pathfinding.SetWall(cell.Item1, cell.Item2, false);
+
+        Destroy(gameObject);
+    }
+}
