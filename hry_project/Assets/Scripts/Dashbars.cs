dae976bce9e657fd4c752d0da3edb282d0d82ee9
Cyril Janecek <janeccyr@fel.cvut.cz>
2020-11-06 16:15:48 +0100

    Show three dash sliders by default

--- hry_project/Assets/Scripts/Dashbars.cs
+++ hry_project/Assets/Scripts/Dashbars.cs
@@ -6,9 +6,16 @@ public class Dashbars : MonoBehaviour
 {
     public GameObject dashbar;
     private int dasbarCount;
+    private List<GameObject> dashbarList;
 
     public void InitializeDashbars(int count)
     {
-        Instantiate(dashbar, transform);
+        dashbarList = new List<GameObject>();
+        for (int i = 0; i < count; i++)
+        {
+            GameObject currDashbar = Instantiate(dashbar, transform);
+            currDashbar.transform.Translate(new Vector3(i - 1, 0, 0));
+            dashbarList.Add(currDashbar);
+        }
     }
 }
