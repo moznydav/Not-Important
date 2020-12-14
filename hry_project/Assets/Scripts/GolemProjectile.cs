8537bf683fa84dd2f6533cf08ca9239ba814ab17
Matěj Gargula <gargumat@fel.cvut.cz>
2020-12-14 19:26:23 +0100

    Golem implemented
    updated golem enemy
    added golem projectile

---
+++ hry_project/Assets/Scripts/GolemProjectile.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemProjectile : MonoBehaviour
+{
+    private float damage = 20f;
+    private bool followPlayer;
+    Collider2D dmgArea;
+    GameObject player;
+
+    private void Awake()
+    {
+        dmgArea = GetComponent<Collider2D>();
+        dmgArea.enabled = false;
+        followPlayer = true;
+        player = GameObject.FindWithTag(Constants.PLAYER_TAG);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+            Stats stats = other.GetComponent<Stats>();
+
+            if (stats)
+            {
+                stats.DealDamage(damage);
+            }
+            // Debug.Log("HIT " + other.name);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followPlayer)
+        {
+            Follow();
+        }
+    }
+
+    private void Follow()
+    {
+        transform.position = player.transform.position;
+    }
+
+    private void StopFollow()
+    {
+        followPlayer = false;
+    }
+
+    private void ActivateDamageArea()
+    {
+        dmgArea.enabled = true;
+    }
+
+    private void KillProjectile()
+    {
+        Destroy(gameObject);
+    }
+
+
+    public void SetDamage(float damage)
+    {
+        this.damage = damage;
+    }
+
+   
+}
