6613c32e98c56eb77fe59d4c6948c2090dc4e164
Matěj Gargula <gargumat@fel.cvut.cz>
2021-01-02 21:28:47 +0100

    End game update

--- hry_project/Assets/Scripts/EnemySpawner.cs
+++ hry_project/Assets/Scripts/EnemySpawner.cs
@@ -12,6 +12,7 @@ public class EnemySpawner : MonoBehaviour
 
     public IEnumerator Spawn(GameObject[] types, int runsDone)
     {
+
         gameManager = FindObjectOfType<GameManager>();
         Vector2 spawnPosition;
         for (int i = 0; i < types.Length; i++) {
@@ -19,8 +20,8 @@ public class EnemySpawner : MonoBehaviour
             gameManager.currentEnemyCount++;
             yield return new WaitForSeconds(Random.Range(0.5f, 1.2f));
             GameObject enemy = Instantiate(types[i], spawnPosition, Quaternion.identity);
-            enemy.GetComponent<Stats>().damage += enemy.GetComponent<Stats>().damage * (runsDone + 2);
-            enemy.GetComponent<Stats>().maxHealth += enemy.GetComponent<Stats>().maxHealth * (runsDone + 2);
+            enemy.GetComponent<Stats>().damage += enemy.GetComponent<Stats>().damage * Mathf.Pow(2,runsDone) ;
+            enemy.GetComponent<Stats>().maxHealth += enemy.GetComponent<Stats>().maxHealth * Mathf.Pow(2, runsDone);
             enemy.GetComponent<Stats>().HealToMax();
         }
     }
